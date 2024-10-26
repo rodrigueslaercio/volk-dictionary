97977c326d69c27b108da95d589ed4c8d8d87405
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2024-10-26 13:45:45 -0300

    fix hotwire with update

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,29 +1,32 @@
 require "httparty"
 
-class ApiController < ApplicationController 
+class ApiController < ApplicationController
   def index
   end
 
   def create
-  if params[:input_text].to_s.split.size == 1
     deepl_response = DeepL.api(params[:input_text])
-    yandex_response = YandexDictionary.api(params[:input_text])
+    audio_response = GoogleAudio.audio(params[:input_text])
 
-    response = {
-      deepL: deepl_response,
-      yandex: yandex_response
-    }
-  else
-    deepl_response = DeepL.api(params[:input_text])
-    response = {
-      deepL: deepl_response
-    }
-  end
+    if params[:input_text].to_s.split.size == 1
+      yandex_response = YandexDictionary.api(params[:input_text])
+
+      response = {
+        deepL: deepl_response,
+        yandex: yandex_response,
+        audio: audio_response
+      }
+    else
+      response = {
+        deepL: deepl_response,
+        audio: audio_response
+      }
+    end
 
     respond_to do |format|
-    format.turbo_stream {
-        render turbo_stream: turbo_stream.replace("result", partial: "result", locals: { result: response })
-    }
+      format.turbo_stream {
+        render turbo_stream: turbo_stream.update("result", partial: "result", locals: { result: response })
+      }
     end
   end
 end
---
+++ lib/google_audio.rb
@@ -0,0 +1,12 @@
+require "httparty"
+require "cgi"
+
+class GoogleAudio
+  def self.audio(text)
+    encoded_text = CGI.escape(text)
+
+    response = HTTParty.get("https://translate.google.com.vn/translate_tts?ie=UTF-8&q=#{encoded_text}&tl=ru&client=tw-ob")
+
+    response.body
+  end
+end
