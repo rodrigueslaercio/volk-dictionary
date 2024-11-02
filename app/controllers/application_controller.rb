4853ab1dcdaff4265681847670a547d82c1c0a34
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2024-11-02 12:19:18 -0300

    add user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :logged_in_user
+
+  def logged_in_user
+    @logged_in_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+  end
+
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else 
+      flash[:alert] = "Login failed, incorrect username or password"
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
